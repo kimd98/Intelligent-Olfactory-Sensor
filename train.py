dc29e13d3fd7b2a0b77031966a18ad245f255a46
Dayoung Lena Kim <dayoung.kim@altium.com>
2021-02-09 16:53:05 -0800

    dataset created

--- train.py
+++ train.py
@@ -2,14 +2,14 @@ import os
 import numpy as np
 import tensorflow as tf
 
-# names of data types
+# constant data info (you may change this)
 data1 = 'freshair'
 data2 = 'citrus'
 data3 = 'peppermint'
 data4 = 'fish'
 data_num = 4
 
-# data path
+# data file path
 data_dir = 'data'
 data_path1 = data_dir + '/' + data1
 data_path2 = data_dir + '/' + data2
@@ -23,15 +23,17 @@ dataset2 = os.listdir(data_path2)
 dataset3 = os.listdir(data_path3)
 dataset4 = os.listdir(data_path4)
 
-# lists of data labels (rank-1 tensors)
-label1 = np.full(len(dataset1), data1)
-label2 = np.full(len(dataset2), data2)
-label3 = np.full(len(dataset3), data3)
-label4 = np.full(len(dataset4), data4)
+# lists of data labels
+label1 = [data1] * len(dataset1)
+label2 = [data2] * len(dataset2)
+label3 = [data3] * len(dataset3)
+label4 = [data4] * len(dataset4)
+total_num = len(label1) + len(label2) + len(label3) + len(label4)
 
-# create a label set and convert to a rank-2 tensor
-train_label = (label1, label2, label3, label4)
-train_label = np.stack(train_label)
+# convert the label list to a rank-3 tensor (1 x 1 x data_num)
+train_label = label1 + label2 + label3 + label4
+train_label = np.array(train_label)
+train_label = np.reshape(train_label, (total_num, 1, 1))
 train_label = tf.constant(train_label)
 
 # generate a list of 2D data set arrays
@@ -49,9 +51,12 @@ for file in dataset4:
     data_file = open(os.path.join(data_path4, file))
     data_3D.append(np.genfromtxt(data_file, delimiter=","))
 
-# generate a 3D data array and convert to a rank-3 tensor
+# convert the 3D array to a rank-3 tensor (data_num x 16 x 16)
 data_3D = np.array(data_3D)
-train_data = tf.convert_to_tensor(data_3D, tf.int16)
+train_feature = tf.convert_to_tensor(data_3D, tf.int16)
 
-# data_num X 16 X 16
-print(train_data.shape)
+# combine two tensors into one dataset object
+train_dataset = tf.data.Dataset.from_tensor_slices((train_feature, train_label))
+print(train_feature.shape)
+print(train_label.shape)
+print(train_dataset.element_spec)
