53b5ea883fec2b75a1f29b54887b7f315a531677
Lena Kim <dayoung.kim@altium.com>
2021-02-22 09:33:00 -0800

    data set updated

--- sgp40.py
+++ sgp40.py
@@ -7,7 +7,7 @@ import adafruit_sgp40
 import numpy as np
 
 # reading # of sample for each dataset
-sample = 60
+sample = 300
 i2c = busio.I2C(board.SCL, board.SDA)
 sgp = adafruit_sgp40.SGP40(i2c)
 data_arr = np.empty(shape=(sample,16), dtype=np.int8)
@@ -35,17 +35,33 @@ def stat(dataNum):
         time.sleep(0.1)
 
 # pick a proper datapath
-type = input("Choose type (freshair/citrus/peppermint/fish): ")
+type = input("Choose type (freshair/citrus/peppermint/fish/banana/garlic): ")
+filepath = os.path.join('data', type)
+condition = ' '
+
+if type == 'banana':
+    condition = input("Choose condition (BA_container/BA_air): ")
+if type == 'garlic':
+    condition = input("Choose condition (garlic_container/garlic_air): ")
 if type == 'freshair':
     condition = input("Choose condition (FA_room/FA_outside/FA_outtoin/FA_intoout): ")
+if type == 'fish':
+    condition = input("Choose condition (fish_container/fish_withair): ")
+if type == 'citrus' :
+    condition = input("Choose condition (citrus_perfume/citrus_mandarine): ")
+if type == 'peppermint':
+    condition = input("Choose condition (PM_container/PM_air): ")
 
+filepath = os.path.join(filepath, condition)
+
+# csv file name represents the data type
+filepath = os.path.join(filepath, type)
 stat(sample)
 saved = np.asarray(data_arr)
-filepath = 'data/' + type + '/' + condition
 i = 0
 while os.path.exists(filepath + "%s.csv" % i):
     i = i + 1
 
-filename = type + str(i) + '.csv'
-np.savetxt(os.path.join(filepath, filename), saved, delimiter=',')
-print("Data file created !")
+filename = filepath + str(i) + '.csv'
+np.savetxt(filename, saved, delimiter=',')
+print("Data file created ! Please check",  filename)
